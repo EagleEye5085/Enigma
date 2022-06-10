6c386b858d9ad9e446b292edeefead5e188665f1
Thomas Turner <thomasturner482@yahoo.com>
2022-06-10 18:02:55 -0500

    bassic decryption

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -13,4 +13,9 @@ RSpec.describe Enigma do
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
   end
 
+  it "can decrypt a message" do
+    expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq({decryption: "hello world", key: "02715", date: "040895"})
+  end
+
+
 end
