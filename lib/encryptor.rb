0b18d646aa28c6e66d92076683a8607b3de8ad18
Thomas Turner <thomasturner482@yahoo.com>
2022-06-10 21:10:51 -0500

    basic encryptor complete

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -35,22 +35,20 @@ class Encryptor
       index += 1
       c
     else
-
       if index == 0
         index += 1
-        @letter_array.rotate(c.ord - 97).rotate(  @offsets[:a_offset])[0]
+        @letter_array.rotate(c.ord - 97).rotate(@offsets[:a_offset])[0]
       elsif index == 1
         index += 1
-        @letter_array.rotate(c.ord - 97).rotate(  @offsets[:b_offset])[0]
+        @letter_array.rotate(c.ord - 97).rotate(@offsets[:b_offset])[0]
       elsif index == 2
         index += 1
-        @letter_array.rotate(c.ord - 97).rotate(  @offsets[:c_offset])[0]
+        @letter_array.rotate(c.ord - 97).rotate(@offsets[:c_offset])[0]
       elsif index == 3
         index = 0
-        @letter_array.rotate(c.ord - 97).rotate(  @offsets[:d_offset])[0]
+        @letter_array.rotate(c.ord - 97).rotate(@offsets[:d_offset])[0]
         end
       end
-      # require 'pry'; binding.pry
     end.join
     output = Hash.new
     output[:encryption] = encryption
