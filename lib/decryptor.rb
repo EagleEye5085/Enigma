f20bd96f529db6740606a1a4674da1e708475da5
Thomas Turner <thomasturner482@yahoo.com>
2022-06-10 23:03:04 -0500

    add test for offsets generator

--- lib/decryptor.rb
+++ lib/decryptor.rb
@@ -1,6 +1,6 @@
 class Decryptor
 
-  attr_reader :message, :key, :date, :letter_array, :keys
+  attr_reader :message, :key, :date, :letter_array, :keys, :offsets
 
   def initialize(message, key, date)
     @message = message
@@ -8,6 +8,7 @@ class Decryptor
     @date = date
     @letter_array = ("a".."z").to_a << " "
     @keys = Hash.new
+    @offsets = Hash.new
   end
 
   def key_generator
@@ -15,6 +16,14 @@ class Decryptor
     @keys[:b_key] = @key[1..2].to_i
     @keys[:c_key] = @key[2..3].to_i
     @keys[:d_key] = @key[3..4].to_i
+  end
+
+  def offset_generator
+    offset = (@date.to_i ** 2).to_s[-4..-1]
+    @offsets[:a_offset] = (@keys[:a_key] + offset[0].to_i)
+    @offsets[:b_offset] = (@keys[:b_key] + offset[1].to_i)
+    @offsets[:c_offset] = (@keys[:c_key] + offset[2].to_i)
+    @offsets[:d_offset] = (@keys[:d_key] + offset[3].to_i)
   end
 
 end
