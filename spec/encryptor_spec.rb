9009eb7738f2eccdabf733d8a88cb0853a0ffe46
Thomas Turner <thomasturner482@yahoo.com>
2022-06-10 20:04:24 -0500

    add offset generator

--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -9,10 +9,18 @@ RSpec.describe Encryptor do
     expect(@encryptor).to be_a Encryptor
   end
 
+  
+
   it "can generate keys" do
     @encryptor.key_generator
     expect(@encryptor.keys).to eq({:a_key=>2, :b_key=>27, :c_key=>71, :d_key=>15})
-  end 
+  end
+
+  it "can generate offsets" do
+    @encryptor.key_generator
+    @encryptor.offset_generator
+    expect(@encryptor.offsets).to eq({:a_offset=>3, :b_offset=>27, :c_offset=>73, :d_offset=>20})
+  end
 
   it "can encrypt a message" do
     # require 'pry'; binding.pry
