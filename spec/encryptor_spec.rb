b29ee8b9c0bdd44821a834d4037557abc9eb4a49
Thomas Turner <thomasturner482@yahoo.com>
2022-06-10 19:22:50 -0500

    basic encryptor

--- spec/encryptor_spec.rb
+++ spec/encryptor_spec.rb
@@ -1,1 +1,15 @@
 require './lib/encryptor'
+
+RSpec.describe Encryptor do
+  before :each do
+    @encryptor = Encryptor.new("hello world", "02715", "040895")
+  end
+
+  it "is an instance of an encryptor" do
+    expect(@encryptor).to be_a Encryptor
+  end
+
+  it "can encrypt a message" do
+    expect(@encryptor.encrypt).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
+end
