364301231f18f439fc01ccea1a64557cae368347
Thomas Turner <thomasturner482@yahoo.com>
2022-06-11 21:27:10 -0500

    add tests for enigma

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -14,8 +14,20 @@ RSpec.describe Enigma do
     expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
   end
 
+  it " can encrypt a message without a date" do
+    expect(@enigma.encrypt("hello world", "02715")).not_to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
+
+  it " can encrypt a message without a date or a key" do
+    expect(@enigma.encrypt("hello world")).not_to eq({encryption: "keder ohulw", key: "02715", date: "040895"})
+  end
+
   it "can decrypt a message" do
     expect(@enigma.decrypt("keder ohulw", "02715", "040895")).to eq({decryption: "hello world", key: "02715", date: "040895"})
+  end
+
+  it "can decrypt a message without a date" do
+    expect(@enigma.decrypt("keder ohulw", "02715")).not_to eq({decryption: "hello world", key: "02715", date: "040895"})
   end
 
 
