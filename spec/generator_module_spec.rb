b0e74e314ce4937635f1d93e637204de035ed685
Thomas Turner <thomasturner482@yahoo.com>
2022-06-11 20:52:03 -0500

    add test for offset generator

--- spec/generator_module_spec.rb
+++ spec/generator_module_spec.rb
@@ -4,7 +4,7 @@ require './lib/encryptor'
 
 RSpec.describe GeneratorModule do
   before :each do
-    @encryptor = Encryptor.new(message = nil, key = ("01234"), date = nil)
+    @encryptor = Encryptor.new(message = nil, key = ("01234"), date = ("090588"))
   end
 
   it "can generate a 5 digit key" do
@@ -14,12 +14,18 @@ RSpec.describe GeneratorModule do
 
   it "can generate a date" do
     @encryptor.date_generator
-    expect(@encryptor.date.length).to eq 6
+    expect(@encryptor.date).not_to eq "090588"
   end
 
   it "can generate keys" do
     @encryptor.keys_generator
     expect(@encryptor.keys).to eq ({:a_key=>1, :b_key=>12, :c_key=>23, :d_key=>34})
+  end
+
+  it "can generate keys" do
+    @encryptor.keys_generator
+    @encryptor.offset_generator
+    expect(@encryptor.offsets).to eq ({:a_offset=>6, :b_offset=>19, :c_offset=>27, :d_offset=>38})
   end
 
 end
