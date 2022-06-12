e9a0863469b482f2952ab748f2b91e879f651231
Thomas Turner <thomasturner482@yahoo.com>
2022-06-11 20:45:42 -0500

    add test for keys generator

--- spec/generator_module_spec.rb
+++ spec/generator_module_spec.rb
@@ -4,17 +4,22 @@ require './lib/encryptor'
 
 RSpec.describe GeneratorModule do
   before :each do
-    @encryptor = Encryptor.new(message = nil, key = nil, date = nil)
+    @encryptor = Encryptor.new(message = nil, key = ("01234"), date = nil)
   end
 
   it "can generate a 5 digit key" do
     @encryptor.key_generator
-    expect(@encryptor.key.length).to eq 5
+    expect(@encryptor.key).not_to eq "01234"
   end
 
   it "can generate a date" do
     @encryptor.date_generator
     expect(@encryptor.date.length).to eq 6
+  end
+
+  it "can generate keys" do
+    @encryptor.keys_generator
+    expect(@encryptor.keys).to eq ({:a_key=>1, :b_key=>12, :c_key=>23, :d_key=>34})
   end
 
 end
